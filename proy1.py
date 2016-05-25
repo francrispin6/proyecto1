89f3ee9475f7e90544c920400b647522290a214e
francrispin6 <francrispin6@gmail.com>
2016-05-25 13:30:45 -0600

    acabo de hacer el 2

---
+++ proy1.py
@@ -0,0 +1,18 @@
+#Proyecto numero 1
+
+def pro1(a):
+	if a % 2 == 0:
+		print("El numero es par")
+	else:
+		print("El numero es impar")
+
+pro1(100)
+
+#Proyecto numero 2
+
+def pro2(f):
+	a = (f-32)*(5/9)
+	print (a)
+pro2(25)
+
+#Proyecto numero 3

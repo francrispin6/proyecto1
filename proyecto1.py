530b6f4f710c2e03780a44ec69be0c7ed03b33a7
francrispin6 <francrispin6@gmail.com>
2016-05-30 13:36:54 -0600

    1

--- proyecto1.py
+++ proyecto1.py
@@ -1,18 +1,18 @@
 #Proyecto numero 1
 
-def pro1(a):
+def prob_1(a):
 	if a % 2 == 0:
 		return("El numero es par")
 	else:
 		return("El numero es impar")
 
 #Proyecto numero 2
-def pro2(f):
+def prob_2(f):
 	a = (f-32)*(5/9)
 	return(a)
 
 #Proyecto numero 3
-def pro3(b, p):
+def prob_3(b, p):
 	res = 1
 	i = 0
 	for i in range(p):
@@ -20,9 +20,7 @@ def pro3(b, p):
 	return(res)
 
 #Proyecto numero 4
-n = int(input("num: "))
-p = int(input("palabra: "))
-def pro4(n, p):
+def prob_4(n, p):
 	l = len(p)	
 	cas=(n-l)/2
 
